13296188b9db342e0cbfc049568656be7191e74e
Kikonen <silmaril666@gmail.com>
2022-12-04 19:04:10 +0000

    Day 4

---
+++ _4/definitions.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+from typing import Set
+
+
+@dataclass
+class Range:
+    start: int
+    end: int
+
+    @classmethod
+    def from_str(cls, range_string: str) -> "Range":
+        start, end = range_string.split('-')
+        return Range(start=int(start), end=int(end))
+
+    def fully_contains(self, other: "Range") -> bool:
+        return (self.start <= other.start) and (self.end >= other.end)
+
+    def get_overlap(self, other: "Range") -> Set[int]:
+        set_self = set(range(self.start, self.end + 1))
+        set_other = set(range(other.start, other.end + 1))
+        return set_self.intersection(set_other)
+
+
+@dataclass
+class RangePair:
+    first: Range
+    second: Range
+
+    @classmethod
+    def from_string(cls, pair_str: str) -> "RangePair":
+        first_str, second_str = pair_str.split(',')
+        return cls(first=Range.from_str(first_str), second=Range.from_str(second_str))
+    
+    def one_is_contained(self) -> bool:
+        return self.first.fully_contains(self.second) or self.second.fully_contains(self.first)
+
+    def do_overlap(self) -> bool:
+        return len(self.first.get_overlap(self.second)) > 0
---
+++ _4/find.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from typing import List
+from definitions import RangePair
+
+class OverlapFinder:
+
+    def __init__(self) -> None:
+        self._all_pairs: List[RangePair] = self._get_all_pairs()
+
+    def get_number_of_pairs_that_fully_overlap(self):
+        counter = 0
+        for pair in self._all_pairs:
+            if pair.one_is_contained():
+                counter += 1
+        return counter
+
+    def get_number_pairs_that_overlap(self):
+        counter = 0
+        for pair in self._all_pairs:
+            if pair.do_overlap():
+                counter += 1
+        return counter
+
+    
+    def _get_all_pairs(self) -> List[RangePair]:
+        path = Path(__file__).parent / 'input.txt'
+        pairs = []
+        with open(path, 'r') as file:
+            for line in file.read().splitlines():
+                pairs.append(RangePair.from_string(line))
+        return pairs
